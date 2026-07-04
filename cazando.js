2069e9074a2d056ab2315ba65682e65b90fd7d72
NathaliaAldas <nathaliaaldas@gmail.com>
2026-07-03 21:32:27 -0600

    Se implemento funciones graficarComida e iniciarJuego, se añadieron variables y constantes, y se ajustó el onload

--- cazando.js
+++ cazando.js
@@ -1,13 +1,33 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
+let gatoX = 0 
+let gatoY = 0;
+let comidaX = 0
+let comidaY = 0;
+
+const ANCHO_GATO = 100;
+const ALTO_GATO = 100;
+const ANCHO_COMIDA = 50;
+const ALTO_COMIDA = 50;
 
 function graficarGato() {
-    let ancho = 100;
-    let alto = 100;
-    let x = (canvas.width - ancho) / 2;
-    let y = (canvas.height - alto) / 2;
-
     ctx.fillStyle = "orange";
-    ctx.fillRect(x, y, ancho, alto);
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+}
+
+function iniciarJuego() {
+    gatoX = (canvas.width - ANCHO_GATO) / 2;
+    gatoY = (canvas.height - ALTO_GATO) / 2;
+
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+
+    graficarGato();
+    graficarComida();
 }
